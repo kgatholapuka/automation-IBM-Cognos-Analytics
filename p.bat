7e9d268b2d0a4d3af33b98e951ac87fb13b519d1
Kgathola Puka <77323146+kgatholapuka@users.noreply.github.com>
2024-03-14 11:57:47 +0200

    Update p.bat

--- p.bat
+++ p.bat
@@ -2,4 +2,5 @@
 
 "C:\Users\ukt430\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\ukt430\Documents\GitHub\automation-IBM-Cognos-Analytics\IBM _cognos.py" 
 pause
-timeout /t 30/nobreak
+
+"C:\Users\ukt430\AppData\Local\Programs\Python\Python312\python.exe" 
