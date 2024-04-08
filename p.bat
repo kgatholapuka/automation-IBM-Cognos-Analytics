d7fd95915c14730255d3e874082111939f46656f
Kgathola Puka <77323146+kgatholapuka@users.noreply.github.com>
2024-04-08 09:16:06 +0200

    Update p.bat

--- p.bat
+++ p.bat
@@ -1,6 +1,11 @@
 @echo off
 
 "C:\Users\ukt430\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\ukt430\Documents\GitHub\automation-IBM-Cognos-Analytics\IBM _cognos.py" 
+
 pause
 
-"C:\Users\ukt430\AppData\Local\Programs\Python\Python312\python.exe" 
+"C:\Users\ukt430\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\ukt430\Documents\GitHub\automation-IBM-Cognos-Analytics\excel.py"
+
+pause
+
+"C:\Users\ukt430\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\ukt430\Documents\GitHub\automation-IBM-Cognos-Analytics\sqaure.py" 
